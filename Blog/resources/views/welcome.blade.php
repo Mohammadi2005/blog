7b4962751fc221716320d79b6a579f7650d8dda1
amir hossein <amohammadi161183@gmail.com>
2025-08-12 11:25:31 +0330

    welcome

---
+++ Blog/resources/views/welcome.blade.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <script src="js/bootstrap.js"></script>
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <style>
+
+    </style>
+</head>
+<body>
+
+<nav class="navbar navbar-expand-md navbar-dark bg-dark border-bottom border-black w-100">
+    <div class="container">
+        <a class="navbar-brand" href="#">PostIno</a>
+        <!--            <button class="text-center">-->
+        <!--                <span class="navbar-toggler-icon"></span>-->
+        <!--            </button>-->
+        <div class="collapse navbar-collapse">
+            <ul class="navbar-nav ms-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">log in</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">sign up</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+<section class="container d-flex justify-content-center">
+    <div class="text-center m-5 w-75">
+        <h1 class="display-3 m-3">Welcome to PostIno!</h1>
+        <p class="lead">
+            This is the place where you can share your stories, ideas, and experiences with others.
+            Sign up, create posts, and start conversations by commenting on your friends’ writings.
+        </p>
+    </div>
+</section>
+
+</body>
+</html>
